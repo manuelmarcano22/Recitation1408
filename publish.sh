280c05625c71d60ec009814a3683c33b1f808783
Manuel Pichardo Marcano <manuelmarcano22@yahoo.com>
2016-09-05 18:03:49 -0400

    Update

--- publish.sh
+++ publish.sh
@@ -13,6 +13,6 @@ git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
 git commit -m "new version"
-git push origin master gh-pages
+git push --force origin master gh-pages
 git checkout master
 rm -rf /tmp/workspace
